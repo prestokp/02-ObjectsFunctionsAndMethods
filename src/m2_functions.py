165c21ee8f3aa765edcc3c62879bbb435d88d9a0
prestokp <prestokp@rose-hulman.edu>
2018-11-28 11:10:25 -0500

    Traced by hand on yellow paper. Code matches. Of course I still love you! -SpaceX Done.

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Kirk Preston.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -21,6 +21,14 @@ import rosegraphics as rg
 
 
 def main():
+    a=3
+    b=4
+    print(hypotenuse(a,b))
+
+    turtle_function("Red", 10)
+    turtle_function("Blue", 15)
+
+
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
@@ -28,7 +36,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# DONE: 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -36,14 +44,18 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# DONE: 3b.  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
+def hypotenuse(a, b):
+    import math
+    math.sqrt((a **2)+(b **2))
+    return( math.sqrt((a **2)+(b **2)))
 
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -65,14 +77,25 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
+def turtle_function(color, thickness):
+    window = rg.TurtleWindow()
+    elon = rg.SimpleTurtle()
+    elon.pen=rg.Pen("Green", thickness)
+    shotwell = rg.SimpleTurtle()
+    shotwell.pen=rg.Pen(color, 5)
+    elon.forward(100)
+    shotwell.backward(100)
+    window.close_on_mouse_click()
+
+
 
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
